9f6499667f2efb2357e249430f59875fc9c3f701
l45he <lashe@list.ru>
2024-01-24 20:10:58 +0600

    [upd] third commit -> add postrgesql

--- prosrochka/prosrochka/settings.py
+++ prosrochka/prosrochka/settings.py
@@ -63,8 +63,12 @@ WSGI_APPLICATION = 'prosrochka.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prosrochka_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Cska1911',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
@@ -91,9 +95,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
 
 USE_I18N = True
 
